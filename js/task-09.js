6813ae0c4912f0f3a0366f614a946e0daa50d7ac
RomaSamkov <Romansamkov.rs@gmail.com>
2022-03-30 19:32:44 +0300

    add task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  button: document.querySelector('.change-color'),
+  span: document.querySelector('.color'),
+  body: document.body,
+};
+
+refs.button.addEventListener('click', onChangeBgColor);
+
+function onChangeBgColor() {
+  const randomColor = getRandomHexColor();
+  refs.body.style.background = randomColor;
+  refs.span.textContent = randomColor;
+};
