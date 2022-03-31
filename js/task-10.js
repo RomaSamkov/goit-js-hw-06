3fb02b0e8590da74cc815b172c247776235c084e
RomaSamkov <Romansamkov.rs@gmail.com>
2022-03-31 14:38:19 +0300

    add task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,38 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  input: document.querySelector('input'),
+  createBtn: document.querySelector('[data-create]'),
+  clearBtn: document.querySelector('[data-destroy]'),
+  boxes: document.querySelector('#boxes'),
+};
+
+refs.createBtn.addEventListener('click', onCreateBoxes);
+refs.clearBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  const allDivsElements = [];
+  const step = 10;
+  let size = 30;
+
+  for (let i = 0; i < amount; i += 1) {
+    const element = document.createElement('div');
+    element.style.width = `${size}px`;
+    element.style.height = `${size}px`;
+    element.style.background = getRandomHexColor();
+
+    size += step;
+    allDivsElements.push(element);
+  }
+  return refs.boxes.append(...allDivsElements);
+};
+
+function onCreateBoxes() {
+  createBoxes(refs.input.value);
+};
+
+function destroyBoxes() {
+  refs.boxes.innerHTML = '';
+};
